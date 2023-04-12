7a14ae7cea25c00a01f06d3d5b8e113824f4379a
AnEvirk <volv.mc@gmail.com>
2023-04-12 13:37:07 +0300

    add Player class fields

--- src/player/Player.java
+++ src/player/Player.java
@@ -1,5 +1,66 @@
 package player;
 
 public class Player {
+        private int health;
+        private int damage;
+        private int speed;
+        private int endurance;
+        private int firingRange;
+
+        public Player() {
+            this.health = 100;
+            this.damage = 50;
+            this.speed = 5;
+            this.endurance = 10;
+            this.firingRange = 100;
+        }
+
+        public Player(int health, int damage, int speed, int endurance, int firingRange) {
+            this.health = health;
+            this.damage = damage;
+            this.speed = speed;
+            this.endurance = endurance;
+            this.firingRange = firingRange;
+        }
+
+        public int getHealth() {
+            return health;
+        }
+
+        public void setHealth(int health) {
+            this.health = health;
+        }
+
+        public int getDamage() {
+            return damage;
+        }
+
+        public void setDamage(int damage) {
+            this.damage = damage;
+        }
+
+        public int getSpeed() {
+            return speed;
+        }
+
+        public void setSpeed(int speed) {
+            this.speed = speed;
+        }
+
+        public int getEndurance() {
+            return endurance;
+        }
+
+        public void setEndurance(int endurance) {
+            this.endurance = endurance;
+        }
+
+        public int getFiringRange() {
+            return firingRange;
+        }
+
+        public void setFiringRange(int firingRange) {
+            this.firingRange = firingRange;
+        }
 
 }
